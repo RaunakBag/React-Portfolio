1991d6ff60b9ca6b7769fafbb8f1025690551e2d
RaunakBag <raunakbag97@gmail.com>
2021-06-29 20:16:46 +0530

    Started

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import './App.css';
+import PersonalLogo from './PersonalLogo.png';
+
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello World</h1>
+     <h1>Hello World</h1>
+     <img src={PersonalLogo} alt="Logo"/>
     </div>
   );
 }
